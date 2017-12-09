6d0562df9752b21c94f7974072d6c96bf0e0d68a
Gianlu <altomanigianluca@gmail.com>
2017-12-09 15:48:32 +0100

    Left behind some compilation error, oops

--- app/src/main/java/com/gianlu/dnshero/Domain/DiagnosticFragment.java
+++ app/src/main/java/com/gianlu/dnshero/Domain/DiagnosticFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +16,7 @@ import com.gianlu.dnshero.R;
 
 import java.util.ArrayList;
 
-public class DiagnosticFragment extends Fragment implements DiagnosticsAdapter.IAdapter {
+public class DiagnosticFragment extends Fragment {
     private RecyclerViewLayout layout;
 
     public static DiagnosticFragment getInstance(Context context, Domain domain) {
@@ -44,13 +43,8 @@ public class DiagnosticFragment extends Fragment implements DiagnosticsAdapter.I
         }
 
         layout.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        layout.loadListData(new DiagnosticsAdapter(getContext(), diagnostics, this));
+        layout.loadListData(new DiagnosticsAdapter(getContext(), diagnostics));
 
         return layout;
-    }
-
-    @Override
-    public RecyclerView getRecyclerView() {
-        return layout.getList();
     }
 }
