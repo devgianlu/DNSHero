ed4b0702860e25743f34e19044849e081b20093a
Gianlu <altomanigianluca@gmail.com>
2017-12-11 12:41:14 +0100

    Added legend for diagnostic

--- app/src/main/java/com/gianlu/dnshero/Domain/DiagnosticFragment.java
+++ app/src/main/java/com/gianlu/dnshero/Domain/DiagnosticFragment.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.gianlu.commonutils.CommonUtils;
 import com.gianlu.commonutils.RecyclerViewLayout;
 import com.gianlu.dnshero.NetIO.Domain;
 import com.gianlu.dnshero.R;
@@ -17,10 +22,10 @@ import com.gianlu.dnshero.R;
 import java.util.ArrayList;
 
 public class DiagnosticFragment extends Fragment {
-    private RecyclerViewLayout layout;
 
     public static DiagnosticFragment getInstance(Context context, Domain domain) {
         DiagnosticFragment fragment = new DiagnosticFragment();
+        fragment.setHasOptionsMenu(true);
         Bundle args = new Bundle();
         args.putString("title", context.getString(R.string.diagnostic));
         args.putSerializable("diagnostics", domain.diagnostics);
@@ -28,11 +33,37 @@ public class DiagnosticFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.diagnostic, menu);
+    }
+
+    private void showHelpDialog() {
+        if (getContext() == null) return;
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(R.string.help)
+                .setView(R.layout.dialog_diagnostic_help)
+                .setPositiveButton(android.R.string.ok, null);
+
+        CommonUtils.showDialog(getActivity(), builder);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.diagnostic_help:
+                showHelpDialog();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Nullable
     @Override
     @SuppressWarnings("unchecked")
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        layout = new RecyclerViewLayout(inflater);
+        RecyclerViewLayout layout = new RecyclerViewLayout(inflater);
         layout.disableSwipeRefresh();
 
         Bundle args = getArguments();
