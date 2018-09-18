7e8007a7e109fd08ef78cfe89ae7707d28ec6646
Gianlu <altomanigianluca@gmail.com>
2018-09-18 13:11:03 +0200

    Fixed #5 + all classes are now static

--- app/src/main/java/com/gianlu/dnshero/NetIO/Domain.java
+++ app/src/main/java/com/gianlu/dnshero/NetIO/Domain.java
@@ -30,7 +30,7 @@ public class Domain implements Serializable {
         root = new RootNameserver(obj.getJSONObject("parent"));
         authoritative = new DNSRecords(obj.getJSONObject("authoritative"));
         resolver = new DNSRecords(obj.getJSONObject("resolver"));
-        diagnostics = CommonUtils.toTList(obj.getJSONObject("diagnostics").getJSONArray("results"), Diagnostic.class, this);
+        diagnostics = Diagnostic.list(obj.getJSONObject("diagnostics").getJSONArray("results"));
         // TODO: errors field missing
     }
 
@@ -55,7 +55,7 @@ public class Domain implements Serializable {
         }
     }
 
-    public class NS implements Serializable {
+    public static class NS implements Serializable {
         public final String source;
         public final ArrayList<String> nameservers;
         public final Glue glue;
@@ -63,7 +63,7 @@ public class Domain implements Serializable {
         public final DNSRecord<DNSRecord.AEntry> a;
         public final DNSRecord<DNSRecord.AEntry> aaaa;
 
-        public NS(JSONObject obj) throws JSONException {
+        private NS(JSONObject obj) throws JSONException {
             source = obj.getString("source");
             nameservers = CommonUtils.toStringsList(obj.getJSONArray("name-servers"), true);
             glue = new Glue(obj.getJSONObject("glue"));
@@ -71,9 +71,47 @@ public class Domain implements Serializable {
             a = new DNSRecord<>(obj.getJSONObject("a"), DNSRecord.AEntry.class);
             aaaa = new DNSRecord<>(obj.getJSONObject("aaaa"), DNSRecord.AEntry.class);
         }
+
+        public static ArrayList<NS> list(JSONArray array) throws JSONException {
+            ArrayList<NS> list = new ArrayList<>(array.length());
+            for (int i = 0; i < array.length(); i++)
+                list.add(new NS(array.getJSONObject(i)));
+            return list;
+        }
     }
 
-    public class DNSRecords implements Serializable {
+    public static class Glue implements Serializable {
+        public final ArrayList<Entry> v4;
+        public final ArrayList<Entry> v6;
+
+        Glue(JSONObject obj) throws JSONException {
+            if (CommonUtils.isStupidNull(obj, "v4")) v4 = null;
+            else v4 = Entry.list(obj.getJSONArray("v4"));
+
+            if (CommonUtils.isStupidNull(obj, "v6")) v6 = null;
+            else v6 = Entry.list(obj.getJSONArray("v6"));
+        }
+
+
+        public static class Entry implements Serializable {
+            public final String address;
+            public final String name;
+
+            private Entry(JSONObject obj) throws JSONException {
+                address = obj.getString("address");
+                name = obj.getString("name");
+            }
+
+            static ArrayList<Glue.Entry> list(JSONArray array) throws JSONException {
+                ArrayList<Glue.Entry> list = new ArrayList<>(array.length());
+                for (int i = 0; i < array.length(); i++)
+                    list.add(new Glue.Entry(array.getJSONObject(i)));
+                return list;
+            }
+        }
+    }
+
+    public static class DNSRecords implements Serializable {
         public final ArrayList<NS> ns;
         public final SOAEntries soa;
         public final MXEntries mx;
@@ -83,7 +121,7 @@ public class Domain implements Serializable {
         public final CNAMEEntries cname;
 
         DNSRecords(JSONObject obj) throws JSONException {
-            ns = CommonUtils.toTList(obj.getJSONArray("ns"), NS.class, Domain.this);
+            ns = NS.list(obj.getJSONArray("ns"));
             soa = new SOAEntries(obj.getJSONArray("soa"));
             mx = new MXEntries(obj.getJSONArray("mx"));
             a = new AEntries(obj.getJSONArray("a"));
@@ -93,7 +131,7 @@ public class Domain implements Serializable {
         }
     }
 
-    public abstract class DNSRecordsArrayList<E extends DNSRecord.Entry> extends ArrayList<DNSRecord<E>> implements Serializable, Comparator<E> {
+    public static abstract class DNSRecordsArrayList<E extends DNSRecord.Entry> extends ArrayList<DNSRecord<E>> implements Serializable, Comparator<E> {
         DNSRecordsArrayList(JSONArray array, Class<E> entryClass) throws JSONException {
             makeList(array, entryClass);
         }
@@ -137,13 +175,13 @@ public class Domain implements Serializable {
         }
     }
 
-    public class SOAEntries extends DNSRecordsArrayList<DNSRecord.SOAEntry> implements Serializable {
+    public static class SOAEntries extends DNSRecordsArrayList<DNSRecord.SOAEntry> implements Serializable {
         SOAEntries(JSONArray array) throws JSONException {
             super(array, DNSRecord.SOAEntry.class);
         }
     }
 
-    public class MXEntries extends DNSRecordsArrayList<DNSRecord.MXEntry> implements Serializable {
+    public static class MXEntries extends DNSRecordsArrayList<DNSRecord.MXEntry> implements Serializable {
         MXEntries(JSONArray array) throws JSONException {
             super(array, DNSRecord.MXEntry.class);
         }
@@ -154,48 +192,25 @@ public class Domain implements Serializable {
         }
     }
 
-    public class AEntries extends DNSRecordsArrayList<DNSRecord.AEntry> implements Serializable {
+    public static class AEntries extends DNSRecordsArrayList<DNSRecord.AEntry> implements Serializable {
         AEntries(JSONArray array) throws JSONException {
             super(array, DNSRecord.AEntry.class);
         }
     }
 
-    public class TXTEntries extends DNSRecordsArrayList<DNSRecord.TXTEntry> implements Serializable {
+    public static class TXTEntries extends DNSRecordsArrayList<DNSRecord.TXTEntry> implements Serializable {
         TXTEntries(JSONArray array) throws JSONException {
             super(array, DNSRecord.TXTEntry.class);
         }
     }
 
-    public class CNAMEEntries extends DNSRecordsArrayList<DNSRecord.CNAMEEntry> implements Serializable {
+    public static class CNAMEEntries extends DNSRecordsArrayList<DNSRecord.CNAMEEntry> implements Serializable {
         CNAMEEntries(JSONArray array) throws JSONException {
             super(array, DNSRecord.CNAMEEntry.class);
         }
     }
 
-    public class Glue implements Serializable {
-        public final ArrayList<Entry> v4;
-        public final ArrayList<Entry> v6;
-
-        Glue(JSONObject obj) throws JSONException {
-            if (CommonUtils.isStupidNull(obj, "v4")) v4 = null;
-            else v4 = CommonUtils.toTList(obj.getJSONArray("v4"), Entry.class, this);
-
-            if (CommonUtils.isStupidNull(obj, "v6")) v6 = null;
-            else v6 = CommonUtils.toTList(obj.getJSONArray("v6"), Entry.class, this);
-        }
-
-        public class Entry implements Serializable {
-            public final String address;
-            public final String name;
-
-            public Entry(JSONObject obj) throws JSONException {
-                address = obj.getString("address");
-                name = obj.getString("name");
-            }
-        }
-    }
-
-    public class RootNameserver implements Serializable {
+    public static class RootNameserver implements Serializable {
         public final String name;
         public final float rtt;
         public final ArrayList<String> nameservers;
@@ -209,14 +224,14 @@ public class Domain implements Serializable {
         }
     }
 
-    public class Diagnostic implements Serializable {
+    public static class Diagnostic implements Serializable {
         public final String name;
         public final String description;
         public final DiagnosticStatus status;
         public final HashMap<String, Boolean> sources;
         public final String recommendation;
 
-        public Diagnostic(JSONObject obj) throws JSONException {
+        private Diagnostic(JSONObject obj) throws JSONException {
             JSONObject definition = obj.getJSONObject("definition");
             name = definition.getString("name");
             description = definition.getString("description");
@@ -230,6 +245,13 @@ public class Domain implements Serializable {
                 String key = keys.next();
                 sources.put(key, sourcesObj.getBoolean(key));
             }
+        }
+
+        static ArrayList<Diagnostic> list(JSONArray array) throws JSONException {
+            ArrayList<Diagnostic> list = new ArrayList<>(array.length());
+            for (int i = 0; i < array.length(); i++)
+                list.add(new Diagnostic(array.getJSONObject(i)));
+            return list;
         }
     }
 }
