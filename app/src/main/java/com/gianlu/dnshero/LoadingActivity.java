6cc5f2fca77a9e3d06c3407ff9040f966efd4d81
devgianlu <altomanigianluca@gmail.com>
2019-08-05 22:25:11 +0200

    Fixed coloring and layout of loading

--- app/src/main/java/com/gianlu/dnshero/LoadingActivity.java
+++ app/src/main/java/com/gianlu/dnshero/LoadingActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.gianlu.commonutils.CommonUtils;
 import com.gianlu.commonutils.Dialogs.ActivityWithDialog;
@@ -17,11 +21,6 @@ import com.gianlu.dnshero.Favorites.FavoritesAdapter;
 import com.gianlu.dnshero.NetIO.Domain;
 import com.gianlu.dnshero.NetIO.ZoneVisionAPI;
 import com.google.android.material.textfield.TextInputLayout;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class LoadingActivity extends ActivityWithDialog implements ZoneVisionAPI.OnSearch, FavoritesAdapter.Listener {
     private TextInputLayout domain;
@@ -56,37 +55,20 @@ public class LoadingActivity extends ActivityWithDialog implements ZoneVisionAPI
         favorites.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
         favorites.setAdapter(new FavoritesAdapter(this, this));
 
-        final ImageButton search = findViewById(R.id.loading_search);
         form = findViewById(R.id.loading_form);
         loading = findViewById(R.id.loading_loading);
         domain = findViewById(R.id.loading_domain);
         CommonUtils.clearErrorOnEdit(domain);
         CommonUtils.getEditText(domain).setOnKeyListener((v, keyCode, event) -> {
             if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                search.performClick();
+                search();
                 return true;
             }
 
             return false;
         });
 
-        search.setOnClickListener(v -> {
-            String query = CommonUtils.getText(domain);
-            if (query.equals(".")) {
-                loading.setVisibility(View.GONE);
-                form.setVisibility(View.VISIBLE);
-                domain.setError("Root domain isn't supported.");
-                favorites.setVisibility(View.VISIBLE);
-            } else if (!query.isEmpty()) {
-                loading.setVisibility(View.VISIBLE);
-                form.setVisibility(View.GONE);
-                favorites.setVisibility(View.GONE);
-
-                ZoneVisionAPI.get().search(query, null, this);
-
-                ThisApplication.sendAnalytics(Utils.ACTION_SEARCH);
-            }
-        });
+        domain.setEndIconOnClickListener(view -> search());
 
         Uri sharedUri = getIntent().getData();
         if (sharedUri != null) {
@@ -100,6 +82,24 @@ public class LoadingActivity extends ActivityWithDialog implements ZoneVisionAPI
 
                 ThisApplication.sendAnalytics(Utils.ACTION_SEARCH_INTENT);
             }
+        }
+    }
+
+    private void search() {
+        String query = CommonUtils.getText(domain);
+        if (query.equals(".")) {
+            loading.setVisibility(View.GONE);
+            form.setVisibility(View.VISIBLE);
+            domain.setError("Root domain isn't supported.");
+            favorites.setVisibility(View.VISIBLE);
+        } else if (!query.isEmpty()) {
+            loading.setVisibility(View.VISIBLE);
+            form.setVisibility(View.GONE);
+            favorites.setVisibility(View.GONE);
+
+            ZoneVisionAPI.get().search(query, null, this);
+
+            ThisApplication.sendAnalytics(Utils.ACTION_SEARCH);
         }
     }
 
