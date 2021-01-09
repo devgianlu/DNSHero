32cfd99065b4cdbea3d15616089c0954ace49f62
Gianlu <altomanigianluca@gmail.com>
2021-01-09 18:18:21 +0100

    Updated GitHub Actions CI

---
+++ .github/decrypt_secrets.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_GPG_PASS" --output .github/secrets.tar .github/secrets.tar.gpg
+tar xvf .github/secrets.tar -C .github
+cp .github/google-services.json app
